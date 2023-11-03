a7b9a513c93849a2e27072d72e02e2bdf7a29c21
datejada <diego.tejadaarango@tno.nl>
2023-11-03 12:00:55 +0100

    Fix files format

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,32 @@
 using TulipaProfileFitting
 using Documenter
 
-DocMeta.setdocmeta!(TulipaProfileFitting, :DocTestSetup, :(using TulipaProfileFitting); recursive = true)
+DocMeta.setdocmeta!(
+    TulipaProfileFitting,
+    :DocTestSetup,
+    :(using TulipaProfileFitting);
+    recursive = true,
+)
 
 makedocs(;
-  modules = [TulipaProfileFitting],
-  doctest = true,
-  linkcheck = true,
-  authors = "TNO <diego.tejadaarango@tno.nl> and contributors",
-  repo = "https://github.com/TulipaEnergy/TulipaProfileFitting.jl/blob/{commit}{path}#{line}",
-  sitename = "TulipaProfileFitting.jl",
-  format = Documenter.HTML(;
-    prettyurls = get(ENV, "CI", "false") == "true",
-    canonical = "https://TulipaEnergy.github.io/TulipaProfileFitting.jl",
-    assets = ["assets/style.css"],
-  ),
-  pages = [
-    "Home" => "index.md",
-    "Mathematical Formulation" => "formulation.md",
-    "Contributing" => "contributing.md",
-    "Dev setup" => "developer.md",
-    "Reference" => "reference.md",
-  ],
+    modules = [TulipaProfileFitting],
+    doctest = true,
+    linkcheck = true,
+    authors = "TNO <diego.tejadaarango@tno.nl> and contributors",
+    repo = "https://github.com/TulipaEnergy/TulipaProfileFitting.jl/blob/{commit}{path}#{line}",
+    sitename = "TulipaProfileFitting.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://TulipaEnergy.github.io/TulipaProfileFitting.jl",
+        assets = ["assets/style.css"],
+    ),
+    pages = [
+        "Home" => "index.md",
+        "Mathematical Formulation" => "formulation.md",
+        "Contributing" => "contributing.md",
+        "Dev setup" => "developer.md",
+        "Reference" => "reference.md",
+    ],
 )
 
 deploydocs(; repo = "github.com/TulipaEnergy/TulipaProfileFitting.jl", push_preview = true)
--- src/model.jl
+++ src/model.jl
@@ -8,9 +8,7 @@ This function takes a vector of profile values and the total hours in the year.
 It fits the profile values to the mean value of the profile and the total hours
 in the year. It returns the optimization model and the coefficient of the scaling.
 """
-function fit_profile(profile_values::Vector{Float64},
-                     target_mean::Float64)
-
+function fit_profile(profile_values::Vector{Float64}, target_mean::Float64)
     total_hours = length(profile_values)
 
     # Create a model
@@ -20,7 +18,11 @@ function fit_profile(profile_values::Vector{Float64},
     @variable(model, x >= 0)
 
     # Create the objective function
-    @NLobjective(model, Min, (sum((profile_values[i])^x for i in 1:total_hours) - target_mean*total_hours)^2)
+    @NLobjective(
+        model,
+        Min,
+        (sum((profile_values[i])^x for i = 1:total_hours) - target_mean * total_hours)^2
+    )
 
     # Solve the model
     optimize!(model)
@@ -30,6 +32,4 @@ function fit_profile(profile_values::Vector{Float64},
 
     # Return the coefficient
     return coefficient
-
 end
-
